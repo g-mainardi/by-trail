aea529afc41b1a26d700fae66b4fb9492549b828
noodles <elvis.perlika@studio.unibo.it>
2025-11-27 11:55:00 +0100

    feat: init Vite project for client +  login page

---
+++ client/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import Login from './pages/login/Login.vue';
+</script>
+
+<template>
+  <Login />
+</template>
+
+<style scoped>
+.page-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
+</style>
