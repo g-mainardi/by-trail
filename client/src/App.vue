04210f28a61fa3ba83cfc9497d72179748e8e38a
elvis-perlika <elvis.perlika@studio.unibo.it>
2025-11-30 10:49:29 +0100

    fix: remove unused style

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,12 +4,3 @@
 <template>
   <RouterView />
 </template>
-
-<style scoped>
-.page-wrapper {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  min-height: 100vh;
-}
-</style>
