4d4fbf2c05eb544fbf843514e9f255085a90043f
elvis-perlika <elvis.perlika@studio.unibo.it>
2025-12-05 12:51:07 +0100

    fix: update import paths for components in main.ts to use alias + move `SignupForm` inside `blocks` package

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,10 +1,10 @@
+import App from '@/App.vue'
+import Hero from '@/pages/hero/Hero.vue'
+import Login from '@/pages/login/Login.vue'
+import Signup from '@/pages/register/Signup.vue'
 import { createApp } from 'vue'
 import { createI18n } from 'vue-i18n'
 import { createRouter, createWebHistory } from 'vue-router'
-import App from './App.vue'
-import Hero from './pages/hero/Hero.vue'
-import Login from './pages/login/Login.vue'
-import Signup from './pages/register/Signup.vue'
 import './style.css'
 
 const i18n = createI18n({
