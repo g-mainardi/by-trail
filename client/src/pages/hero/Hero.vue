d3b20af90091cd618a51dfe14ed42a2a60cbf2f7
g-mainardi <giosue.mainardi@studio.unibo.it>
2025-12-09 00:33:35 +0100

    feat: custom hero page based on logging

--- client/src/pages/hero/Hero.vue
+++ client/src/pages/hero/Hero.vue
@@ -8,7 +8,14 @@ import Button from '@/components/ui/button/Button.vue';
 import SelectLanguage from '@/components/ui/select/SelectLanguage.vue';
 import { useI18n } from 'vue-i18n';
 import { RouterLink } from 'vue-router';
+import { useAuthStore } from '@/stores/auth';
+
 const { t } = useI18n()
+const authStore = useAuthStore()
+
+const handleLogout = () => {
+  authStore.logout()
+}
 </script>
 
 <template>
@@ -21,15 +28,32 @@ const { t } = useI18n()
     </div>
 
     <div class="flex flex-col items-center">
-      <h1 class="text-4xl font-bold text-white">{{ t("welcome") }}</h1>
-      <div class="mt-6 flex gap-4">
-      <RouterLink to="/login">
-        <Button>{{ t("login") }}</Button>
-      </RouterLink>
-      <RouterLink to="/signup">
-        <Button variant="secondary">{{ t("signup") }}</Button>
-      </RouterLink>
+      
+      <div v-if="authStore.user" class="text-center flex flex-col items-center gap-6">
+        <h1 class="text-4xl font-bold text-white">
+          {{t("welcome_as") }} {{ authStore.user.name }}!
+        </h1>
+        <p class="text-white/80 text-lg">
+          {{t("logged_as") }} {{ authStore.user.email }} 
+        </p>
+        
+        <Button variant="destructive" @click="handleLogout">
+          {{t("logout")}}
+        </Button>
       </div>
+
+      <div v-else class="flex flex-col items-center">
+        <h1 class="text-4xl font-bold text-white">{{ t("welcome") }}</h1>
+        <div class="mt-6 flex gap-4">
+          <RouterLink to="/login">
+            <Button>{{ t("login") }}</Button>
+          </RouterLink>
+          <RouterLink to="/signup">
+            <Button variant="secondary">{{ t("signup") }}</Button>
+          </RouterLink>
+        </div>
+      </div>
+
     </div>
   </div>
 </template>
@@ -39,17 +63,26 @@ const { t } = useI18n()
   "en": {
     "welcome": "Welcome to By Trail",
     "login": "Log In",
-    "signup": "Sign Up"
+    "signup": "Sign Up",
+    "logout": "Log Out",
+    "logged_as": "You are logged in as ",
+    "welcome_as": "Welcome, "
   },
   "it": {
     "welcome": "Benvenuto su By Trail",
     "login": "Accedi",
-    "signup": "Registrati"
+    "signup": "Registrati",
+    "logout": "Disconnettiti",
+    "logged_as": "Sei connesso come",
+    "welcome_as": "Benvenuto, "
   },
   "es": {
     "welcome": "Bienvenido a By Trail",
     "login": "Iniciar sesión",
-    "signup": "Registrarse"
+    "signup": "Registrarse",
+    "logout": "Cerrar sesión",
+    "logged_as": "Has iniciado sesión como",
+    "welcome_as": "Bienvenido, "
   }
 }
 </i18n>
