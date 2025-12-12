8c79b5a0c1cadb62347a35a14e5c347c73b74724
Matteo Zaccarini <matteo.zaccarini4@studio.unibo.it>
2025-12-12 15:19:39 +0100

    refactor: seed-database (#11)
    * refactor: improve seed.js reability
    
    * chore: resolve conflicts
    
    * refactor: improve seed.js readability

--- server/seed.js
+++ server/seed.js
@@ -1,39 +1,38 @@
 import mongoose from "mongoose";
+import fs from 'fs';
+import path from 'path';
+
 import { User, Admin, Bivacco, Trail, Image, FavBivacco, FavTrail, Reservation, Setting, Notify } from './src/models/models.js'
 
 const MONGO_URI_LOCAL = 'mongodb://localhost:27017/by_trail'
 
+const loadData = (fileName) => {
+    try {
+        const filePath = path.join(process.cwd(), 'src', 'data', fileName);
+        const rawData = fs.readFileSync(filePath, 'utf-8');
+        console.log(`Successfully loaded ${fileName}`);
+        return JSON.parse(rawData);
+    } catch (error) {
+        console.error(`Error loading data from ${fileName}:`, error);
+        return [];
+    }
+}
+
 const seedData = async () => {
     try {
-        await mongoose.connect(MONGO_URI_LOCAL); // Look for explanation
-        
+        await mongoose.connect(MONGO_URI_LOCAL);
+
+        const usersData = loadData('users.json');
+        const bivaccosData = loadData('bivaccos.json');
+
         await User.deleteMany({});
         await Bivacco.deleteMany({});
-        await Trail.deleteMany({});
 
-        const users = await User.insertMany([
-            { name: 'Jeff Bezossss', email: 'jeff_bezos@gmail.com' },
-            { name: 'Mark Zuckenberg', email: 'markinho_brazil@yahoo.com' }
-        ]);
+        const users = await User.insertMany(usersData.users);
+        const bivaccos = await Bivacco.insertMany(bivaccosData.bivaccos);
 
-        const bivaccos = await Bivacco.insertMany([
-            { 
-                name: 'Bivacco Bedin',
-                region: 'Veneto',
-                mountainRange: 'Dolomiti',
-                altitude: 2210,
-                capacity: 9,
-            },
-            {
-                name: 'Bivacco Vigolana',
-                region: 'Trentino - Alto Adige',
-                mountainRange: 'Prealpi Venete',
-                altitude: '2030',
-                capacity: '8',
-            },
-        ]);
-
-        process.exit();
+        await mongoose.connection.close();
+        process.exit(0);
 
     } catch (error) {
         console.error('Error seeding database:', error);
