c4937d32ea1111bba2e1bee140fc4bad9ae3c022
elvis-perlika <elvis.perlika@studio.unibo.it>
2025-12-14 11:19:16 +0100

    feat: add new thiigs icon assets and `MyIcon` component to wrap them

---
+++ client/src/components/ui/icons/MyIcon.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+defineProps<{
+  path: string,
+  size?: number
+}>();
+</script>
+
+<template>
+  <img :src="path" :width="size ? size * 16 : 24" :height="size ? size * 16 : 24" alt="icon" />
+</template>
