8c1fc19590b697c9b124db58260ef76fa17ac2dc
g-mainardi <giosue.mainardi@studio.unibo.it>
2026-01-07 16:05:53 +0100

    feat: implement activities page tabs for empty saved routes, saved bivouacs, and proposal form

---
+++ client/src/pages/activities/tabs/SavedRoutes.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import H1 from '@/layouts/typography/H1.vue';
+import { useI18n } from 'vue-i18n';
+const { t } = useI18n();
+</script>
+
+<template>
+  <H1 :text="t('saved_routes')" />
+</template>
+
+<i18n>
+{
+  "en": {
+    "saved_routes": "Saved Routes"
+  },
+  "it": {
+    "saved_routes": "Percorsi Salvati"
+  },
+  "es": {
+    "saved_routes": "Rutas Guardadas"
+  }
+}
+</i18n>
