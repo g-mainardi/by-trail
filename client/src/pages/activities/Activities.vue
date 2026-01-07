bd0c5bb5688f8afc21ca7cfdab7dd52bd4286df8
g-mainardi <giosue.mainardi@studio.unibo.it>
2026-01-07 22:40:58 +0100

    refactor(client): headers and descriptions for activities

--- client/src/pages/activities/Activities.vue
+++ client/src/pages/activities/Activities.vue
@@ -35,18 +35,36 @@ const { t } = useI18n();
 
     <TabsContent value="routes">
       <Card class="container">
+        <div>
+          <H1 :text="t('saved_routes')" />
+          <p class="text-muted-foreground">
+            {{ t('your_routes_description') }}
+          </p>
+        </div>
         <SavedRoutes />
       </Card>
     </TabsContent>
 
     <TabsContent value="bivouacs">
       <Card class="container">
+        <div>
+          <H1 :text="t('saved_bivouacs')" />
+          <p class="text-muted-foreground">
+            {{ t('your_bivouacs_description') }}
+          </p>
+        </div>
         <SavedBivouacs />
       </Card>
     </TabsContent>
 
     <TabsContent value="proposal">
       <Card class="container">
+        <div>
+          <H1 :text="t('make_a_proposal')" />
+          <p class="text-muted-foreground">
+            {{ t('make_a_proposal_description') }}
+          </p>
+        </div>
         <ProposalForm />
       </Card>
     </TabsContent>
@@ -59,21 +77,39 @@ const { t } = useI18n();
     "your_activities_description": "Manage and review your past activities.",
     "tab_routes": "Routes",
     "tab_bivouacs": "Bivouacs",
-    "tab_proposal": "Proposal"
+    "tab_proposal": "Proposal",
+    "saved_routes": "Saved Routes",
+    "your_routes_description": "Review and manage your saved routes.",
+    "saved_bivouacs": "Saved Bivouacs",
+    "your_bivouacs_description": "Review and manage your saved bivouacs.",
+    "make_a_proposal": "Make a Proposal",
+    "make_a_proposal_description": "Have an idea for a new route or bivouac? Send us your proposal and we'll consider it for future inclusion on our platform.",
   },
   "it": {
     "your_activities": "Le tue attività",
     "your_activities_description": "Gestisci e rivedi le tue attività passate.",
     "tab_routes": "Percorsi",
     "tab_bivouacs": "Bivacchi",
-    "tab_proposal": "Proposta"
+    "tab_proposal": "Proposta",
+    "saved_routes": "Percorsi Salvati",
+    "your_routes_description": "Rivedi e gestisci i tuoi percorsi salvati.",
+    "saved_bivouacs": "Bivacchi Salvati",
+    "your_bivouacs_description": "Rivedi e gestisci i tuoi bivacchi salvati.",
+    "make_a_proposal": "Fai una Proposta",
+    "make_a_proposal_description": "Hai un'idea per un nuovo percorso o bivacco? Inviaci la tua proposta e la prenderemo in considerazione per future inclusioni nella nostra piattaforma.",
   },
   "es": {
     "your_activities": "Tus actividades",
     "your_activities_description": "Gestiona y revisa tus actividades pasadas.",
     "tab_routes": "Rutas",
     "tab_bivouacs": "Bivouacs",
-    "tab_proposal": "Propuesta"
+    "tab_proposal": "Propuesta",
+    "saved_routes": "Rutas Guardadas",
+    "your_routes_description": "Revisa y gestiona tus rutas guardadas.",
+    "saved_bivouacs": "Bivouacs Guardados",
+    "your_bivouacs_description": "Revisa y gestiona tus bivouacs guardados.",
+    "make_a_proposal": "Haz una Propuesta",
+    "make_a_proposal_description": "¿Tienes una idea para una nueva ruta o bivouac? Envíanos tu propuesta y la consideraremos para futuras inclusiones en nuestra plataforma.",
   }
 }
 </i18n>
--- client/src/pages/activities/tabs/SavedBivouacs.vue
+++ client/src/pages/activities/tabs/SavedBivouacs.vue
@@ -1,23 +1,17 @@
 <script setup lang="ts">
-import H1 from '@/layouts/typography/H1.vue';
 import { useI18n } from 'vue-i18n';
 const { t } = useI18n();
 </script>
 
-<template>
-  <H1 :text="t('saved_bivouacs')" />
-</template>
+<template></template>
 
 <i18n>
 {
   "en": {
-    "saved_bivouacs": "Saved Bivouacs"
   },
   "it": {
-    "saved_bivouacs": "Bivacchi Salvati"
   },
   "es": {
-    "saved_bivouacs": "Bivouacs Guardados"
   }
 }
 </i18n>
