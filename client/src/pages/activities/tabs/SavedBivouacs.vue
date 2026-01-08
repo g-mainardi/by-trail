a7ffb5e94ad0056caf6cb1f5c74b57a02f754113
g-mainardi <giosue.mainardi@studio.unibo.it>
2026-01-08 20:59:27 +0100

    feat(client): implement favorite bivouacs visualization

--- client/src/pages/activities/tabs/SavedBivouacs.vue
+++ client/src/pages/activities/tabs/SavedBivouacs.vue
@@ -1,9 +1,33 @@
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n';
+import { ref } from 'vue';
+
+import type { Bivouac } from '@/stores/bivouacs';
+import { useFavoriteStore } from '@/stores/favorites';
+
+import BivouacCard from './BivouacCard.vue';
+
 const { t } = useI18n();
+
+const favoriteStore = useFavoriteStore();
+const favoriteBivouacsResponse = ref(
+  await favoriteStore.getFavoriteBivouacs().catch((error) => {
+    console.error('Error fetching favorite bivouacs:', error);
+    return { bivouacs: [] };
+  })
+);
+const favoriteBivouacs = ref<Bivouac[]>(
+  favoriteBivouacsResponse.value.bivouacs
+);
 </script>
 
-<template></template>
+<template>
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    <div v-for="bivouac in favoriteBivouacs" :key="bivouac._id">
+      <BivouacCard :bivouac="bivouac" />
+    </div>
+  </div>
+</template>
 
 <i18n>
 {
