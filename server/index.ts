620c2739cba0fc2a14bb001456d4b72ec0b2ce5c
Giosuè Mainardi <giosue.mainardi@studio.unibo.it>
2026-01-13 12:54:07 +0100

    chore: improve server access control with CORS whitelist and script development mode  (#37)
    * feat(server): update login response to include status code
    
    * chore(server): enhance CORS configuration with whitelist and dev mode
    
    * chore(server): update package.json scripts for environment configuration with cross-env
    
    * chore(server): update CORS whitelist to conditionally include SWAGGER_ORIGIN
    
    * chore(server): explicitly allow localhost in CORS for security reasons
    
    * chore(server): fix start script to include index.ts for production

--- server/index.ts
+++ server/index.ts
@@ -9,7 +9,16 @@ import userRoutes from './src/routes/userRoutes.ts';
 // Environment variables setup
 const PORT = process.env.PORT || 3000; // Default port for Express
 const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:5173'; // Vite default port
+const SWAGGER_ORIGIN =
+  process.env.SWAGGER_ORIGIN ||
+  (process.env.NODE_ENV === 'development'
+    ? 'http://localhost:8080'
+    : undefined);
+const whitelist = [CLIENT_ORIGIN];
 
+if (SWAGGER_ORIGIN) {
+  whitelist.push(SWAGGER_ORIGIN);
+}
 const app = express();
 
 // Middleware setup
@@ -27,7 +36,26 @@ if (CLIENT_ORIGIN === '*') {
 
 app.use(
   cors({
-    origin: CLIENT_ORIGIN,
+    origin: function (origin, callback) {
+      // To allow server-to-server requests and tools like Postman/Curl (that don't send Origin header)
+      if (!origin || whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        // Allow localhost in development mode
+        if (
+          process.env.NODE_ENV === 'development' &&
+          origin &&
+          (origin.startsWith('vscode-webview://') ||
+            origin.startsWith('http://localhost:') ||
+            origin.startsWith('https://localhost:'))
+        ) {
+          return callback(null, true);
+        }
+
+        console.log('Blocked by CORS:', origin); // useful for debugging
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true,
   })
 );
