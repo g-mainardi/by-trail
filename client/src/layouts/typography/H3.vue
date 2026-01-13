45138833ba77754fb654512b8361e5374bff97d8
g-mainardi <giosue.mainardi@studio.unibo.it>
2026-01-13 12:55:05 +0100

    feat(client): add H3 component with TypeScript setup

---
+++ client/src/layouts/typography/H3.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h3
+    class="scroll-m-20 border-b pb-2 text-2xl font-semibold tracking-tight first:mt-0"
+  >
+    <slot />
+  </h3>
+</template>
