59c087696ceff531e943a14b2f5005f18a52bd08
g-mainardi <giosue.mainardi@studio.unibo.it>
2026-01-14 19:06:18 +0100

    feat(client): update typography components to support noMargin prop

--- client/src/layouts/typography/H1.vue
+++ client/src/layouts/typography/H1.vue
@@ -1,8 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+defineProps<{ noMargin?: boolean; text: string }>();
+</script>
 
 <template>
   <h1
-    class="scroll-m-20 text-left text-4xl font-extrabold tracking-tight text-balance"
+    :class="[
+      'scroll-m-20 text-left text-4xl font-extrabold tracking-tight text-balance',
+      noMargin ? 'm-0' : 'first:mt-0',
+    ]"
   >
     <slot />
   </h1>
--- client/src/layouts/typography/H3.vue
+++ client/src/layouts/typography/H3.vue
@@ -1,8 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+defineProps<{ noMargin?: boolean }>();
+</script>
 
 <template>
   <h3
-    class="scroll-m-20 border-b pb-2 text-2xl font-semibold tracking-tight first:mt-0"
+    :class="[
+      'scroll-m-20 border-b pb-2 text-2xl font-semibold tracking-tight',
+      noMargin ? 'm-0' : 'first:mt-0',
+    ]"
   >
     <slot />
   </h3>
