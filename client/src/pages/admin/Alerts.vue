8ed2995010fb2eeafe38a59911bb94f070cf0207
g-mainardi <giosue.mainardi@studio.unibo.it>
2026-01-16 11:13:40 +0100

    feat(client): standardize admin and activities tabs layout

--- client/src/pages/admin/Alerts.vue
+++ client/src/pages/admin/Alerts.vue
@@ -1,23 +1,9 @@
 <script setup lang="ts">
-import H1 from '@/layouts/typography/H1.vue';
-import { useI18n } from 'vue-i18n';
-const { t } = useI18n();
+// import { useI18n } from 'vue-i18n';
+// const { t } = useI18n();
 </script>
 
-<template>
-  <H1>{{ t('alerts_management') }}</H1>
-</template>
+<template></template>
 
 <i18n>
-{
-  "en": {
-    "alerts_management": "Alerts Management"
-  },
-  "it": {
-    "alerts_management": "Gestione Segnalazioni"
-  },
-  "es": {
-    "alerts_management": "Gestión de Alertas"
-  }
-}
 </i18n>
