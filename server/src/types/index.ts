6b266919cdc71372e5f60d28762660a431ee71d7
elvis-perlika <elvis.perlika@studio.unibo.it>
2026-01-18 19:03:58 +0100

    feat: add intention interface

--- server/src/types/index.ts
+++ server/src/types/index.ts
@@ -124,3 +124,11 @@ export interface Proposal {
   description: string;
   locality: string;
 }
+
+export interface Intention {
+  _id: string;
+  userId: string;
+  bivouacId: string;
+  reservedPlaces: number;
+  reservationDate: Date;
+}
