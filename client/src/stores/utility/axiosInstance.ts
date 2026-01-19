29dfc064c81aa45a073b353f4fd87252d8745ad9
elvis-perlika <elvis.perlika@studio.unibo.it>
2026-01-19 17:02:01 +0100

    fix: correct baseURL assignment logic in axios instance

--- client/src/stores/utility/axiosInstance.ts
+++ client/src/stores/utility/axiosInstance.ts
@@ -3,7 +3,10 @@ import axios from 'axios';
 
 const api = axios.create({
   // VITE_API_URL is used on production, fallback to '/api' for local development
-  baseURL: import.meta.env.VITE_API_URL + '/api' || '/api',
+
+  baseURL: import.meta.env.VITE_API_URL
+    ? import.meta.env.VITE_API_URL + '/api'
+    : '/api',
   headers: {
     'Content-Type': 'application/json',
   },
