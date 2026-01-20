069ab5c86b28ded351ed5af1bb5fc38a1dcc820a
g-mainardi <giosue.mainardi@studio.unibo.it>
2026-01-20 19:34:00 +0100

    feat(server): protect delete profile API with authentication

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import { deleteAccount, login, signup } from '../controllers/authController.js';
-import { authRateLimiter } from '../middleware/authMiddleware.js';
+import { authRateLimiter, protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-// Define endpoints
+// /api/auth
 router.post('/signup', authRateLimiter, signup);
 router.post('/login', authRateLimiter, login);
-router.post('/delete', authRateLimiter, deleteAccount);
+router.post('/delete', protect, deleteAccount);
 
 export default router;
