d037fcd45454a9081de69cac7fe1883204fe938f
g-mainardi <giosue.mainardi@studio.unibo.it>
2026-01-21 15:58:01 +0100

    fix: missing items and comments

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -3,7 +3,7 @@ import { deleteAccount, login, signup } from '../controllers/authController.js';
 import { authRateLimiter, protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
-router.use(authRateLimiter); // Apply rate limiting to all routes under
+router.use(authRateLimiter); // Apply rate limiting to all routes under /api/auth
 
 // /api/auth
 router.post('/signup', signup);
